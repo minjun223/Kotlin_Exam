4e93d3fe361c6a5c309d5aa07b9b2b96f883fae3
김민준 <80810346+minjun223@users.noreply.github.com>
2022-07-03 10:03:56 +0900

    Kotlin

---
+++ main/java/Kotlin/10/Mutable.kt
@@ -0,0 +1,26 @@
+package Kotlin
+
+import kotlinx.coroutines.processNextEventInCurrentThread
+
+fun main(args: Array<String>){
+    val mutableList:MutableCollection<Int> = mutableListOf(1,2,4,2,3,2,5)
+    println(mutableList)
+
+    mutableList.add(1)
+    println(mutableList)
+
+    mutableList.addAll(listOf(3,2,4))
+    println(mutableList)
+
+    mutableList.remove(1)
+    println(mutableList)
+
+    mutableList.removeAll(listOf(1,2))
+    println(mutableList)
+
+    mutableList.removeAll(listOf(3,5))
+    println(mutableList)
+
+    mutableList.clear()
+    println(mutableList)
+}
