9cc7af85fdfff6449a3e74709650b33b39191c44
김민준 <80810346+minjun223@users.noreply.github.com>
2022-07-10 14:18:40 +0900

    Kotlin

---
+++ main/java/Kotlin/10/Regex.kt
@@ -0,0 +1,13 @@
+package Kotlin
+
+fun main(args: Array<String>)
+{
+    val regex = Regex("[0-9]+")
+    val str = "4324235"
+    val str2 = "324 6546 5432"
+
+    println(regex matches str)
+    println(regex matches str2)
+
+    println(regex.replace(str2, "숫자"))
+}
